fd67beabd9ca33420278dc26636ca611560ea5fc
EvanEbert <evan.ebert17@gmail.com>
2021-08-27 00:08:00 -0500

    added working local storage whenever you click the button + alert saying 'saved!'

--- Assets/script.js
+++ Assets/script.js
@@ -1,73 +1,86 @@
-var timeBlockDiv = document.getElementById('daniel');
+$(document).ready(function () {
+    var timeBlockDiv = document.getElementById('daniel');
 
-var timeblocksArray = [];
-timeblocksArray.length = 22;
-var tArrLength = timeblocksArray.length;
+    var timeblocksArray = [];
+    timeblocksArray.length = 22;
+    var tArrLength = timeblocksArray.length;
 
-var currentDayDiv = document.getElementById('currentDay');
-var today = new Date();
-var dd = String(today.getDate()).padStart(2, '0');
-var mm = String(today.getMonth() + 1).padStart(2, '0');
-var yyyy = today.getFullYear();
-today = mm + '/' + dd + '/' + yyyy;
-currentDayDiv.append(today);
+    var currentDayDiv = document.getElementById('currentDay');
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0');
+    var yyyy = today.getFullYear();
+    today = mm + '/' + dd + '/' + yyyy;
+    currentDayDiv.append(today);
 
-var currentTime = new Date();
-var time = currentTime.getHours();
+    var currentTime = new Date();
+    var time = currentTime.getHours();
 
-function createTimeblocks() {
-    for (let i = 9; i < tArrLength; i++) {
-        if (i <= 22) {
-            let timeblockText = document.createElement('p');
-            let timeblockField = document.createElement('input');
-            let timeblockButton = document.createElement('button');
-            let timeblockCard = document.createElement('div');
-            
-            timeblockCard.classList.add('timeDiv');
+    function createTimeblocks() {
+        for (let i = 9; i < tArrLength; i++) {
+            if (i <= 22) {
+                let timeblockText = document.createElement('p');
+                let timeblockField = document.createElement('input');
+                let timeblockButton = document.createElement('button');
+                let timeblockCard = document.createElement('div');
 
-            if (i < time) {
-                timeblockField.classList.add('past');
-            } else if (i === time) {
-                timeblockField.classList.add('present');
-            } else if (i > time) {
-                timeblockField.classList.add('future');
+                timeblockCard.classList.add('timeDiv');
+                timeblockCard.setAttribute('id', `${i}`)
+
+                if (i < time) {
+                    timeblockField.classList.add('past');
+                } else if (i === time) {
+                    timeblockField.classList.add('present');
+                } else if (i > time) {
+                    timeblockField.classList.add('future');
+                }
+                timeblockField.setAttribute('type', 'text')
+                timeblockField.classList.add('description')
+                if (i <= 11) {
+                    timeblockText.textContent = `${i}AM`;
+                } else if (i === 12) {
+                    timeblockText.textContent = `12PM`
+                }
+                else {
+                    timeblockText.textContent = `${i - 12}PM`
+                }
+                timeblockText.classList.add('hour')
+                timeblockButton.textContent = "Save";
+                timeblockButton.classList.add('saveBtn');
+                //timeblockButton.setAttribute('id', `${i}`);
+                timeBlockDiv.append(timeblockCard);
+                timeblockCard.append(timeblockText);
+                timeblockCard.append(timeblockField);
+                timeblockCard.append(timeblockButton);
             }
-            timeblockField.setAttribute('type', 'text')
-            timeblockField.setAttribute('id', `${i}`)
-            if (i <= 11) {
-                timeblockText.textContent = `${i}AM`;
-            } else if (i === 12) { 
-                timeblockText.textContent = `12PM` 
-            }
-            else {
-                timeblockText.textContent = `${i - 12}PM`
-            }
-            timeblockText.classList.add('hour')
-            timeblockButton.textContent = "Save";
-            timeblockButton.classList.add('saveBtn');
-            timeblockButton.setAttribute('id', `${i}`);
-            timeBlockDiv.append(timeblockCard);
-            timeblockCard.append(timeblockText);
-            timeblockCard.append(timeblockField);
-            timeblockCard.append(timeblockButton);
         }
     }
-}
 
-createTimeblocks();
-var buttons = document.querySelectorAll('button');
-var blockText = document.querySelectorAll('input');
-console.log(buttons)
-console.log(blockText)
-for (let i = 0; i < buttons.length; i++) {
-    const allBtns = buttons[i];
-    const blockTextindex = blockText[i]
-    
-    allBtns.addEventListener('click', function(){
-        // for (let j = 0; j < blockText.length; j++) {
-        //     const blockTextindex = blockText[j]
-        //     console.log('hello')
-        // }
-        
-})
-};
+    createTimeblocks();
+    var buttons = document.querySelectorAll('button');
+    var blockText = document.querySelectorAll('input');
+
+    for (let i = 0; i < buttons.length; i++) {
+        const allBtns = buttons[i];
+
+        allBtns.addEventListener('click', function () {
+                localStorage.setItem(`${i}`, blockText[i].value)
+                alert('Saved!');
+                console.log(localStorage)
+        })
+    }
+
+    $('#9 .description').val(localStorage.getItem('0'));
+    $('#10 .description').val(localStorage.getItem('1'));
+    $('#11 .description').val(localStorage.getItem('2'));
+    $('#12 .description').val(localStorage.getItem('3'));
+    $('#13 .description').val(localStorage.getItem('4'));
+    $('#14 .description').val(localStorage.getItem('5'));
+    $('#15 .description').val(localStorage.getItem('6'));
+    $('#16 .description').val(localStorage.getItem('7'));
+    $('#17 .description').val(localStorage.getItem('8'));
+    $('#18 .description').val(localStorage.getItem('9'));
+    $('#19 .description').val(localStorage.getItem('10'));
+    $('#20 .description').val(localStorage.getItem('11'));
+    $('#21 .description').val(localStorage.getItem('12'));
+});
